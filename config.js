ee183be1be11bda5e37400dc71f45a0b3dd114c2
austinbrown-opspark <austinworb@gmail.com>
2020-08-16 20:07:43 -0500

    tsdfsdf

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-// require('dotenv').config();
-// const { PORT } = process.env;
-
 const OAUTH_CLIENT_ID = "314042252557-0ckq9tej6vdl0mtrfcftila7tth46h22.apps.googleusercontent.com";
 // const OAUTH_CLIENT_ID = "1026149554561-9k189ri77spejnogk2nkshkbd0vaivgh.apps.googleusercontent.com"
 const YOUTUBE_API_KEY = "AIzaSyAnIDqtMssJwNgo3kFeIRPkQ8GZ9T8cgCU";
