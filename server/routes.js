8218d8ca2303b96cb7a7a1a1f17778dd094ca3a8
Evan Taft <evtaft22@gmail.com>
2020-08-19 15:48:58 -0500

    "fixed error in routes"

--- server/routes.js
+++ server/routes.js
@@ -14,7 +14,6 @@ const {
     PartySongUser,
     Invitee
   } = require('../db/database.js');
-const { InvalidConnectionError } = require('sequelize/types');
 
 // SEND ACCESS CODE
 router.post('/invites', (req, res) => {
