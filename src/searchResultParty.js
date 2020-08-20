c7bc41e11516c726fff1896862d282000994a089
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-19 23:24:40 -0500

    Added party specific components

---
+++ src/searchResultParty.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ListGroup from "react-bootstrap/ListGroup";
+import ListEntryParty from './listEntry.js';
+
+// Song search list
+const SearchResultsParty = ({ videos, listClickHandler }) => {
+  return (
+    <ListGroup style={{ padding: "5%" }}> 
+    <div>
+      {videos.map((video) => (
+        <ListEntryParty video={video} listClickHandler={listClickHandler} />
+      ))}
+    </div>
+    </ListGroup>
+  );
+};
+
+export default SearchResultsParty;
